ab1603a85ea938b0414427fe78327d365aee2793
hirsch <gery.hirschfeld@w3tec.ch>
2021-02-03 08:04:58 +0100

    chore(): init

---
+++ packages/angular-output-target/src/types.ts
@@ -0,0 +1,21 @@
+export interface OutputTargetAngular {
+  componentCorePackage?: string;
+  directivesProxyFile: string;
+  directivesArrayFile?: string;
+  directivesUtilsFile?: string;
+  valueAccessorConfigs?: ValueAccessorConfig[];
+  excludeComponents?: string[];
+}
+
+export type ValueAccessorTypes = 'text' | 'radio' | 'select' | 'number' | 'boolean';
+
+export interface ValueAccessorConfig {
+  elementSelectors: string | string[];
+  event: string;
+  targetAttr: string;
+  type: ValueAccessorTypes;
+}
+
+export interface PackageJSON {
+  types: string;
+}
---
+++ packages/vue-2-output-target/src/index.ts
@@ -0,0 +1,2 @@
+export { vue2OutputTarget } from './plugin';
+export type { OutputTargetVue, ComponentModelConfig } from './types';
